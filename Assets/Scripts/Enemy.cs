e1ae620ebec08c2f9ff5d2a806ea34be6b54b743
isaacMullen <43121643+isaacMullen@users.noreply.github.com>
2024-11-10 12:18:51 -0400

    Not Sure What I Did

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,7 +24,7 @@ public class Enemy : Actor {
 
     public override Vector3 GetMovementDirection()
     {
-        Vector3 directionToPlayer = ((Player.instance.transform.position - transform.position));
+        Vector3 directionToPlayer = (Player.instance.transform.position - transform.position);
         directionToPlayer.y = 0.0f;
         return directionToPlayer.normalized;
     }
